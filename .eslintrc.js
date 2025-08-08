573e13a7491954258778c5669bcc93ae4e93580c
shitposts <66158890+shift-jis@users.noreply.github.com>
2025-08-09 08:14:23 +0900

    Update ESLint config for TypeScript and Prettier
    Simplifies the ESLint configuration by removing unused plugins and rules, updating parser options to use the latest ECMAScript version, and streamlining the extends array for better compatibility with TypeScript and Prettier.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,8 @@
 module.exports = {
     parser: "@typescript-eslint/parser",
     env: { node: true },
-    parserOptions: { ecmaVersion: 6, sourceType: "module" },
-    plugins: ["@typescript-eslint", "eslint-plugin-prettier"],
-    rules: {
-        semi: ["error", "never"],
-        "no-unused-vars": "off",
-        quotes: ["error", "double"],
-        "@typescript-eslint/no-unused-vars": "error",
-        "quote-props": ["error", "as-needed"],
-        "prefer-arrow-callback": ["error"],
-        "prettier/prettier": "error",
-    },
-    extends: ["eslint-config-prettier", "plugin:prettier/recommended", "eslint:recommended", "plugin:@typescript-eslint/eslint-recommended"],
+    parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+    plugins: ["@typescript-eslint"],
+    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"],
+    rules: { semi: ["error", "never"], quotes: ["error", "double"], "quote-props": ["error", "as-needed"], "prefer-arrow-callback": ["error"] },
 }
